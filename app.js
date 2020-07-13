dbbc8a695876cb14d207015eaf156a067ef20258
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-07-13 19:21:36 -0400

    added try/catch

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+topURL = "https://api.jikan.moe/v3/top/anime/1/airing"
+
+
 //create try and catch script for API
+const topAnime = async () => {
+  const topAnime = topURL
+  try {
+    const response = await axios.get(topAnime)
+    console.log(response)
+  } catch (error) {
+    console.error(`This is my ${error}`)
+  }
+} 
+topAnime()
 
 
+//Append things to do the should be in the top/trending section
+//The things we would be appending to the DOM are title and image_url 
+//And rank for the top 10 things. 
 
-//Append things to do the dom 
+//Appending things to the other DOM in the divs of search and userlist 
+
+
+//create another script to search and find randomly (POST MVP)
 
