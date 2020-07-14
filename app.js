927592628078ea5cd787cd222a5d77c45e74ccc4
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-07-14 15:01:50 -0400

    foreach loop completed

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 topURL = "https://api.jikan.moe/v3/top/anime/1/airing"
 searchAnime = " https://api.jikan.moe/v3/search/anime/{id}(/request)"
 
-
-
-//To access the data in the top API it should be response.data.top[index]
-
-const renderTopAnime = (anime) => {
-  anime.foreach((animes) => {
+const topFiftyAnime = (animes) => {
+  animes.forEach((anime) => {
+    let animeTitle = document.createElement('h4')
+    animeTitle.innerText = anime.title
+    document.querySelector('.topandtrending').append(animeTitle)
     let poster = document.createElement('img')
-    poster.src = animes.image_url
+    poster.src = anime.img_url
+    document.querySelector('.topandtrending').append(poster)
   })
+  console.log(animes)
+  
+}
 
 
 
-//create try and catch script for API
+//create try and catch script for top anime function
 const topAnime = async () => {
   const topAnime = topURL
   try {
     const response = await axios.get(topURL)
-    console.log(response.data.top[0])
+    // console.log(response.data.top)
+    topAnimu(response.data.top)
   } catch (error) {
     console.error(`This is my ${error}`)
   }
@@ -27,18 +31,10 @@ topAnime()
 
 
 
-//Testing things to append to DOM in topandtrending 
-// const topAnimu = () => {
-//   let current = document.querySelector('.topandtrending')
-//   let newElement = document.createElement('p')
-//   newElement.appendChild(document.createTextNode('Plus Ultra!'))
-//   current.appendChild(newElement)
 
-// }
-// topAnimu();
-//Append things to do the should be in the top/trending section
-//The things we would be appending to the DOM are title and image_url 
-//And rank for the top 10 things. 
+
+
+
 
 //Appending things to the other DOM in the divs of search and userlist 
 
